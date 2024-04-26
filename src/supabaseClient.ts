4afa262f2e58f2e7ed602fd0836f6b00cd8638c3
David Gardner <davidgardner002@gmail.com>
2024-04-26 14:44:53 -0600

    App running successfully.  Hardcoded env vars in supabaseClient.ts. Fixed avatar code using edits from PR request to original repo.  Run w: `ionic serve`

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string
+//const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string
+//const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string
+const supabaseUrl = 'https://kxhrjhwntqxyfyfqjsnn.supabase.co' as string
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4aHJqaHdudHF4eWZ5ZnFqc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMTk4ODIsImV4cCI6MjAyOTU5NTg4Mn0.9KggIEZ0FaaqI9MgmMi5J9C6QvLP3mX50FYk7_Mgl1Y' as string
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+//export const supabase = createClient('https://kxhrjhwntqxyfyfqjsnn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4aHJqaHdudHF4eWZ5ZnFqc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMTk4ODIsImV4cCI6MjAyOTU5NTg4Mn0.9KggIEZ0FaaqI9MgmMi5J9C6QvLP3mX50FYk7_Mgl1Y')
+
